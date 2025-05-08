158eaaccc46b71fba1b0f346aa022da897cbe54d
hinkleydev <azz@hinkley.dev>
2025-05-08 22:14:08 +0100

    FIX: Incorrect APP type used

--- flaskr/token.py
+++ flaskr/token.py
@@ -8,7 +8,7 @@ CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 
 @token.get("/auth")
 def redirect_auth():
-    return redirect("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID)
+    return redirect("https://github.com/login/oauth/authorize?scope=repo&client_id=" + CLIENT_ID)
 
 @token.get("/callback")
 def authenticate():
@@ -16,8 +16,4 @@ def authenticate():
     r = requests.post("https://github.com/login/oauth/access_token", 
         json={"client_id" : CLIENT_ID, "client_secret" : CLIENT_SECRET, "code" : code},
         headers={"accept" : "application/json"})
-    access_token = r.json()["access_token"]
-    user = requests.get("https://api.github.com/user",
-        headers={"Accept" : "application/json", "Content-Type" : "application/json", "Authorization" : "Bearer " + access_token})
-    userValues = user.json()
-    return jsonify({"user": userValues["login"], "token": access_token})
+    return jsonify(r.json())
